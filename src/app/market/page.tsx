c18c63377eafdf6ba6bce61a056d6cbaf12dba53
Klesti Selimaj <klesti.selimay@icloud.com>
2026-02-18 02:13:49 +0100

    Simple market chart

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -3,6 +3,7 @@
 import Navbar from "@/components/layout/Navbar";
 import Statusbar from "@/components/layout/Statusbar";
 import MarketSidebar from "./components/market-sidebar/MarketSidebar";
+import Chart from "./components/market-sidebar/Chart";
 
 export default function Market() {
   return (
@@ -10,7 +11,7 @@ export default function Market() {
       <Navbar />
       <section className="w-full h-full flex flex-row">
         <div className="w-full h-svh border-r border-border">
-            
+          <Chart />
         </div>
         <MarketSidebar />
       </section>
